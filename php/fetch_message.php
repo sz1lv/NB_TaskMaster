ac7560a2e8e324277888f50ffd7fef3738e12ab5
sz1lv <sz1lv.vincze@gmail.com>
2019-03-28 18:31:01 +0100

    Commit #2

--- php/fetch_message.php
+++ php/fetch_message.php
@@ -2,6 +2,7 @@
 
 require_once('../config/connect.php');
 session_start();
+//Üzenetküldésnél a küldő és címzett meghatározása
 echo fetch_message($_SESSION['felhasznalo_id'], $_POST['kinek_id'], $db_conn);
 
 ?>
